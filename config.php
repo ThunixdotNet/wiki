419e69a0e3771fe89a71f4533664cbaef3564cb4
ubergeek <ubergeek@yourtilde.com>
2019-06-30 21:10:38 -0400

    Update 'config.php'

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $site_name="Thunix Wiki";
 
 //Root for the site, in a browser
-$site_root="";
+$site_root="https://wiki.thunix.net";
 
 //Local base root for app files
 $doc_root="/var/www/wiki.thunix.net/";
